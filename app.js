4730bd702ce42eb8bb0eba1d5a57ca8fd26901f3
kmvp <kari_oc3@hotmail.com>
2020-05-17 19:46:11 -0500

    nuevo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ translate.engine = 'yandex'
 translate.key = 'trnsl.1.1.20200504T063735Z.24589d8003959227.37c1a92d9512eb5fb889b2551a75871afb2b71f7'
 //const geocodificacion = require("./app_geo")
 //const pronostico = require("./app_pro")
-
+//const PORT = 3000
 // S e t t i n g s
 app.set('port', 3000)
 app.set('view engine', 'ejs')
@@ -43,14 +43,14 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     const url=`http://api.weatherstack.com/current?access_key=6134446f5a686e709de3199c8d98d3cd&query=${req.body.city}`
     request({url:url, json:true}, (error, response)=>{
-        weather_descript = response.body.current.weather_descriptions[0]
-        translate(weather_descript, {from: 'en', to:'es'}).then(text => {
-            callback(undefined,{
-                weather_descript: text,
+        //weather_descript = response.body.current.weather_descriptions[0]
+        //translate(weather_descript, {from: 'en', to:'es'}).then(text => {
+        //    callback(undefined,{
+        //        weather_descript: text,
                 // temperature: response.body.current.temperature,
                 // precipitation: response.body.current.precip,
                 // temperature_feelslike: response.body.current.feelslike  
-            })
+        //    })
             //console.log("El clima en " + response.body.location.name + ", " + response.body.location.country + " es " + text)
             // weather_description = text
             // datac = response.body.current
@@ -58,8 +58,8 @@ app.post('/', (req, res) => {
             // city = req.body.city
             // res.redirect('/weather')
             
-        })
-        console.log(response)
+        //})
+        //console.log(response)
         //poner la funcion aqui y luego crear una variable con el valor asignado para despues pasarlo a abajo
         weather_description = response.body.current.weather_descriptions[0]
         datac = response.body.current
@@ -85,6 +85,7 @@ app.post('/weather',(req,res)=>{
 
 app.use(express.static('public'))
 
-app.listen(app.get('port'), () => {
-    console.log('Server on port 5000 at http://localhost:', app.get('port'))
-});
+app.listen(process.env.PORT, function() {
+    //console.log('Server on port 3000 at http://localhost:', app.get('port'))
+    console.log('Server on port 3000 at http://localhost:3000')
+})
